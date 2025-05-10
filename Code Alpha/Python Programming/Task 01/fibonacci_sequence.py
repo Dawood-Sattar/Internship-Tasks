14f7e491282083664a3aeb639fc10a332da05bb0
Dawood Sattar <dawoodsattar007@gmail.com>
2025-05-10 16:17:56 +0500

    Add files via upload

---
+++ Code Alpha/Python Programming/Task 01/fibonacci_sequence.py
@@ -0,0 +1,17 @@
+def recur_fibo(n):
+    if n<=1:
+        return n
+    else:
+        return(recur_fibo(n-1)+recur_fibo(n-2))
+
+
+
+noOfTerms=int(input("Enter No of Terms : "))
+
+if noOfTerms<=0:
+    print("Please Enter Positive Integer : ")
+else:
+    print("Fibonacci sequence : ")
+
+    for i in range(noOfTerms):
+        print(recur_fibo(i))
